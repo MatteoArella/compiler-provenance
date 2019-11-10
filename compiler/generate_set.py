b6999fd9a96952e89ff8254197f9f51fec18701c
Matteo Arella <arella.1646983@studenti.uniroma1.it>
2019-11-10 16:49:30 +0100

    Update generate_set.py

--- compiler/generate_set.py
+++ compiler/generate_set.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     DataShuffleSplit(dataset_path='code/train_dataset.jsonl',
             train_output='code/compiler/train.joblib',
             test_output='code/compiler/test.joblib',
-            target_label='opt',
+            target_label='compiler',
             train_description='Train dataset for compiler provenance problem',
             test_description='Test dataset for compiler provenance problem',
             force=False,
